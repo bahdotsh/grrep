f6b5b9cc5938c094f91861cb92ccab6d7d85319d
bahdotsh <appu.yess@gmail.com>
2023-04-07 15:40:12 +0530

    added basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::process;
+
+use grrep::Config;
+
+fn main(){
+	let args: Vec<String> = env::args().collect();
+	
+	let config = Config::build(&args).unwrap_or_else(|err| {
+		println!("Problem parsing arguments: {}", err);
+		process::exit(1);
+	});
+	
+	println!("Searching for {} in {}", config.query, config.file_path);
+	
+	if let Err(e) =	grrep::run(config) {
+		println!("Application error: {e}!");
+		process::exit(1);
+	}
 }
