52925f9f39cef425d8e448abe9740e1d49829e91
bahdotsh <appu.yess@gmail.com>
2023-04-07 20:11:16 +0530

    Writing Error Messages to Standard Error Instead of Standard Output

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,13 @@ fn main(){
 	let args: Vec<String> = env::args().collect();
 	
 	let config = Config::build(&args).unwrap_or_else(|err| {
-		println!("Problem parsing arguments: {}", err);
+		eprintln!("Problem parsing arguments: {}", err);
 		process::exit(1);
 	});
 	
 	
 	if let Err(e) =	grrep::run(config) {
-		println!("Application error: {e}!");
+		eprintln!("Application error: {e}!");
 		process::exit(1);
 	}
 }
